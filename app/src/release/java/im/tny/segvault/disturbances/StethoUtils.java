a8347fcffb1034d6483140b771fb121f5bcac5e7
gbl08ma <gbl08ma@gmail.com>
2017-05-08 23:43:11 +0100

    Fix release builds by removing unnecessary Stheto imports

--- app/src/release/java/im/tny/segvault/disturbances/StethoUtils.java
+++ app/src/release/java/im/tny/segvault/disturbances/StethoUtils.java
@@ -2,9 +2,6 @@ package im.tny.segvault.disturbances;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
-
 /**
  * Created by gabriel on 5/8/17.
  */
