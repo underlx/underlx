51a9246d2edcfc96690caeaebe42ba8263ce8d9a
gbl08ma <gbl08ma@gmail.com>
2017-08-03 10:07:31 +0100

    Always sort disturbance statuses.
    Fixes #33

--- app/src/main/java/im/tny/segvault/disturbances/DisturbanceRecyclerViewAdapter.java
+++ app/src/main/java/im/tny/segvault/disturbances/DisturbanceRecyclerViewAdapter.java
@@ -19,6 +19,8 @@ import android.widget.TextView;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -151,6 +153,12 @@ public class DisturbanceRecyclerViewAdapter extends RecyclerView.Adapter<Disturb
             for(API.Status s : disturbance.statuses) {
                 statuses.add(new Status(new Date(s.time[0] * 1000), s.status, s.downtime));
             }
+            Collections.sort(statuses, new Comparator<Status>() {
+                @Override
+                public int compare(Status o1, Status o2) {
+                    return o1.date.compareTo(o2.date);
+                }
+            });
         }
 
         @Override
