ae0e5e33a92776e4a0084a4ceccd11b784bb286c
gbl08ma <gbl08ma@gmail.com>
2018-09-30 13:30:53 +0100

    Fix blurry icons on Android 4.4 and below
    The vector drawables support library is now used instead of PNG
    generation.
    The trick with the layer_* drawables is to avoid crashes on 4.4 and below
    when drawables are used as background/drawableLeft/drawableTop/etc.

--- app/src/main/java/im/tny/segvault/disturbances/ui/activity/TopActivity.java
+++ app/src/main/java/im/tny/segvault/disturbances/ui/activity/TopActivity.java
@@ -3,6 +3,7 @@ package im.tny.segvault.disturbances.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 import im.tny.segvault.disturbances.LocaleUtil;
 
@@ -11,6 +12,9 @@ import im.tny.segvault.disturbances.LocaleUtil;
  */
 
 public abstract class TopActivity extends AppCompatActivity {
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
